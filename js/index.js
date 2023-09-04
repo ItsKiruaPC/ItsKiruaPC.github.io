42f727e882b85c010cc5788c13c960e37c0e0941
ItsKiruaPC <adrien.fevre31@gmail.com>
2023-09-04 22:27:57 +0200

    ajout du mode téléphone page accueil

--- js/index.js
+++ js/index.js
@@ -18,3 +18,12 @@ window.onscroll = () =>
         }
     })
 }
+
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
